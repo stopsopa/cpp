8b4ea03bb6a9051d54b2a08d5a3c096581cbc76a
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2022-12-11 00:07:26 +0000

    up

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Type a number: ";       // Type a number and press enter
+    cin >> x;                        // Get user input from the keyboard
+    cout << "Your number is: " << x; // Display the input value
+}
